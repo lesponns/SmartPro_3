0c0607f8b193d530f77fcd4333d9a97578a70eb8
lesponns <lesponns@DESKTOP-44MF2L6>
2023-07-20 19:04:51 +0300

    Proje dosyası ekle.

---
+++ SmartPro_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değer kayıt
+            string username;
+
+            //Kullanıcıdan istenen değerin ne olduğu
+            Console.Write("Name: ");
+            //Kullanıcıdan gelen değeri okuma ve text değerine kayıt
+            username = Console.ReadLine();
+            Console.Clear();
+
+            if (true /*Koşul durumu*/)
+            {
+                // Ek koşul durumu ve sonuç yazılır
+            }
+            else if (true)
+            {
+                //else if, 1. koşul yani if'in sağlanmadığı durumda geçerlidir.
+            }
+            else if (true)
+            {
+                //1. ve 2. koşulun sağlanmadığı durumda geçerlidir.
+            }
+            else
+            {
+                //if koşullarının sağlanmadığı durumda son çare olarak geçerlidir.
+            }
+
+
+
+        }
+    }
+}
