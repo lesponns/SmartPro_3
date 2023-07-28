5f986eef8790af192935d0b582768eeaf0a353a3
lesponns <71814927+lesponns@users.noreply.github.com>
2023-07-28 07:01:23 +0300

    Update Program.cs

--- SmartPro_3/Program.cs
+++ SmartPro_3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,32 +12,37 @@ namespace SmartPro_3
         {
             //Değer kayıt
             string username;
-
             //Kullanıcıdan istenen değerin ne olduğu
             Console.Write("Name: ");
             //Kullanıcıdan gelen değeri okuma ve text değerine kayıt
             username = Console.ReadLine();
             Console.Clear();
 
-            if (true /*Koşul durumu*/)
+            if (username == "Lesponns" /*Koşul durumu*/)
             {
                 // Ek koşul durumu ve sonuç yazılır
+                Console.Clear();
+                Console.WriteLine("Welcome back " + username);
             }
-            else if (true)
+            else if (username == "Mustafa")
             {
                 //else if, 1. koşul yani if'in sağlanmadığı durumda geçerlidir.
+                Console.Clear();
+                Console.WriteLine("Welcome back " + username);
             }
-            else if (true)
+            else if (username == "Admin")
             {
                 //1. ve 2. koşulun sağlanmadığı durumda geçerlidir.
+                Console.Clear();
+                Console.WriteLine("Welcome back " + username);
             }
             else
             {
                 //if koşullarının sağlanmadığı durumda son çare olarak geçerlidir.
+                Console.Clear();
+                Console.WriteLine("User not found!");
             }
-
-
-
+            Console.ReadKey();  
         }
     }
 }
